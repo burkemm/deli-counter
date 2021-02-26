cad2157b749445fa9ffa527d59e24d4a9a2c34ca
burkemm <burkemm84@gmail.com>
2021-02-26 15:35:15 -0600

    commit tree f9b8cea6dc7652cee8e79bbd7f828cb7818a3f53

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(people)
+    if people.length == 0
+        puts "The line is currently empty."
+        else
+        first_phrase = "The line is currently:"
+        people.each.with_index(1) do |person, number|
+            first_phrase << " #{number}. #{person}"
+        end
+        puts first_phrase
+    end
+end
+
+def take_a_number(current_line, name)
+    current_line.push(name)
+    puts "Welcome, #{name}. You are number #{current_line.count} in line."
+end
+
+def now_serving(people)
+    if people.length == 0
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{people.shift}."
+    end
+end
